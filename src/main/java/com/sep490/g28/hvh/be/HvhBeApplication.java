c7a02310cf6e87aea0a1ffd6ff1b2e33d9ea9bd9
huyendieu8304 <huyendieu8304@gmail.com>
2026-01-16 21:55:45 +0700

    initial commit

---
+++ src/main/java/com/sep490/g28/hvh/be/HvhBeApplication.java
@@ -0,0 +1,13 @@
+package com.sep490.g28.hvh.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvhBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HvhBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sep490/g28/hvh/be/HvhBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep490.g28.hvh.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvhBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
