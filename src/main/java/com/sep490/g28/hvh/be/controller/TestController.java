0c44d6555512d15c6ed74a237c26b2e0fd6750b6
huyendieu8304 <huyendieu8304@gmail.com>
2026-01-26 16:50:41 +0700

    wip: include test controller to test

---
+++ src/main/java/com/sep490/g28/hvh/be/controller/TestController.java
@@ -0,0 +1,99 @@
+package com.sep490.g28.hvh.be.controller;
+
+import com.sep490.g28.hvh.be.auth.CurrentUserProvider;
+import com.sep490.g28.hvh.be.dto.user.RegisterVolunteerAccountRequest;
+import com.sep490.g28.hvh.be.integration.cache.OtpService;
+import com.sep490.g28.hvh.be.integration.mail.EmailService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/test")
+@Validated
+@RequiredArgsConstructor
+public class TestController {
+
+    @PostMapping(
+            value = "/register",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<String> registerVolunteer(
+            @Valid @ModelAttribute RegisterVolunteerAccountRequest request
+    ) {
+        return ResponseEntity.ok("OK");
+    }
+
+    // test ConstraintViolationException (method-level)
+    @GetMapping("/phone")
+    public ResponseEntity<String> testPhone(
+            @Pattern(
+                    regexp = "^(0|\\+84)(3|5|7|8|9)\\d{8}$",
+                    message = "INVALID_PHONE"
+            )
+            @RequestParam String phone
+    ) {
+        return ResponseEntity.ok("OK");
+    }
+
+    private final CurrentUserProvider currentUserProvider;
+//    @PreAuthorize("hasAnyRole('SYS_ADMIN', 'VOL')")
+    @PreAuthorize("hasRole('SYS_ADMIN')")
+    @GetMapping("/me")
+    public ResponseEntity<Map<String, Object>> me() {
+
+        return ResponseEntity.ok(Map.of(
+                "userId", currentUserProvider.getId(),
+                "email", currentUserProvider.getEmail(),
+                "role", currentUserProvider.getRoleName()
+        ));
+    }
+
+//    private final EmailSenderService emailSenderService;
+//    @GetMapping("/mail")
+//    public ResponseEntity<?> mail() throws MessagingException {
+//
+//        String subject = "Test mail";
+//        String html = "<h1>Mail OK</h1><p>This is a test</p>";
+//
+//        emailSenderService.sendEmail("huyendieu8304@gmail.com", subject, html);
+//
+//        return ResponseEntity.ok("OK");
+//    }
+
+    private final EmailService emailService;
+    @GetMapping("/mail")
+    public ResponseEntity<String> testMail(@RequestParam String email) {
+
+        emailService.sendApproveRegisterVolAccountEmail(email);
+        return ResponseEntity.ok("OK");
+
+    }
+
+    private final OtpService otpService;
+
+    @GetMapping("/otp")
+    public ResponseEntity<String> getOtp() {
+
+        return ResponseEntity.ok(otpService.getVerifyRegisterOtp("huyendieu8304@gmail.com"));
+    }
+
+    @GetMapping("/otp-verify")
+    public ResponseEntity<String> testOtp(@RequestParam String otp) {
+        if (otpService.verifyVerifyRegisterOtp("huyendieu8304@gmail.com", otp)){
+        return ResponseEntity.ok("OK");
+        }
+        return ResponseEntity.ok("oh nooooo");
+    }
+
+    // TODO set up docker and test redis
+
+}
