fe834e97b3472450668991551a39d74fa42d8efe
kiennguyen <kienduccml@gmail.com>
2026-02-24 23:04:54 +0700

    fix: change some validation in RegisterOrganizationRequest dto class

--- src/main/java/com/sep490/g28/hvh/be/dto/organization/RegisterOrganizationRequest.java
+++ src/main/java/com/sep490/g28/hvh/be/dto/organization/RegisterOrganizationRequest.java
@@ -3,6 +3,7 @@ package com.sep490.g28.hvh.be.dto.organization;
 import com.sep490.g28.hvh.be.validation.AllowedFileExtension;
 import com.sep490.g28.hvh.be.validation.ImageFileExtension;
 import com.sep490.g28.hvh.be.validation.OrgType;
+import com.sep490.g28.hvh.be.validation.RequiredField;
 import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -17,26 +18,27 @@ public class RegisterOrganizationRequest {
     @Pattern(regexp = "^\\d{6}$", message = "INVALID_OTP")
     String otp;
 
-    @NotBlank(message = "INVALID_ORG_NAME")
+    @RequiredField(fieldName = "Tên tổ chức")
     String name;
 
-    @NotNull(message = "REQUIRED_FIELD")
+    @RequiredField(fieldName = "Trạng thái đăng ký")
     Boolean dhaRegistered;
 
     @NotBlank(message = "INVALID_ORG_TYPE")
-    @OrgType(message = "Organization type string format must be full uppercase and split by underscores")
+    @OrgType
     String orgType;
 
-    @NotBlank(message = "REQUIRED_FIELD")
+    @RequiredField(fieldName = "Giới thiệu về tổ chức")
     @Length(max = 500)
     String orgIntroduction;
 
+    @Pattern(regexp = "^[A-ZÀ-Ỹ][a-zà-ỹ]*(?:\\s[A-ZÀ-Ỹ][a-zà-ỹ]*)*$", message = "INVALID_FULL_NAME")
     @NotBlank(message = "INVALID_FULL_NAME")
-    @Length(max = 100)
+    @Length(max = 100, message = "INVALID_FULL_NAME")
     String managerFullName;
 
     @NotBlank(message = "INVALID_CID")
-    @Length(max = 12)
+    @Pattern(regexp = "^\\d{12}$", message = "INVALID_CID")
     String managerCid;
 
     @NotBlank(message = "INVALID_PHONE")
@@ -59,10 +61,10 @@ public class RegisterOrganizationRequest {
     @ImageFileExtension(fieldName = "Ảnh cầm căn cước công dân")
     String managerCidHoldingExtension;
 
-    @NotBlank(message = "INVALID_FILES_TYPE")
+    @NotBlank(message = "INVALID_FILE_TYPE")
     @AllowedFileExtension(fieldName = "Những tài liệu liên quan khác")
     String otherEvidencesExtensions;
 
-    @NotBlank(message = "REQUIRED_FIELD")
+    @RequiredField(fieldName = "Lý do đăng ký")
     String applicationReason;
 }
