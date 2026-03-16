86e5556ae453f4cce0fab31b3c73f1e0306f8239
huyendieu8304 <huyendieu8304@gmail.com>
2026-03-16 22:46:59 +0700

    add: field approved_application_count to table event_sessions

--- src/main/java/com/sep490/g28/hvh/be/repository/EventApplicationRepository.java
+++ src/main/java/com/sep490/g28/hvh/be/repository/EventApplicationRepository.java
@@ -2,8 +2,18 @@ package com.sep490.g28.hvh.be.repository;
 
 import com.sep490.g28.hvh.be.entity.EventApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EventApplicationRepository extends JpaRepository<EventApplication, UUID> {
+    @Query(
+            value = """
+                    SELECT e.*
+                    FROM event_applications e
+                    WHERE volunteer_id = :volunteerId AND session_id = :sessionId
+                    """,
+            nativeQuery = true)
+    Optional<EventApplication> getEventApplicationsByVolunteerIdAndSessionId(UUID volunteerId, UUID sessionId);
 }
--- src/main/java/com/sep490/g28/hvh/be/service/impl/EventApplicationServiceImpl.java
+++ src/main/java/com/sep490/g28/hvh/be/service/impl/EventApplicationServiceImpl.java
@@ -1,12 +1,25 @@
 package com.sep490.g28.hvh.be.service.impl;
 
+import com.sep490.g28.hvh.be.auth.CurrentUserProvider;
+import com.sep490.g28.hvh.be.constant.EEventApplicationStatus;
+import com.sep490.g28.hvh.be.entity.Event;
+import com.sep490.g28.hvh.be.entity.EventApplication;
+import com.sep490.g28.hvh.be.entity.EventSession;
+import com.sep490.g28.hvh.be.exception.AppException;
+import com.sep490.g28.hvh.be.exception.errorCodeImpl.EventErrorCode;
+import com.sep490.g28.hvh.be.repository.EventApplicationRepository;
+import com.sep490.g28.hvh.be.repository.EventRepository;
+import com.sep490.g28.hvh.be.repository.EventSessionRepository;
+import com.sep490.g28.hvh.be.repository.VolunteerRepository;
 import com.sep490.g28.hvh.be.service.EventApplicationService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Slf4j
@@ -14,18 +27,57 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class EventApplicationServiceImpl implements EventApplicationService {
+    EventSessionRepository eventSessionRepository;
+    EventApplicationRepository eventApplicationRepository;
+    CurrentUserProvider currentUserProvider;
+    private final EventRepository eventRepository;
+    private final VolunteerRepository volunteerRepository;
+
+    @Transactional
     @Override
     public void applyEventSession(UUID sessionId) {
         //check session existed?
+        EventSession session = eventSessionRepository.findById(sessionId).orElseThrow(
+                () -> new AppException(EventErrorCode.EVENT_SESSION_NOT_EXISTED));
+
+        UUID volunteerId = currentUserProvider.getId();
+
+        //Have ever the volunteer applied for this session yet?
+        if (eventApplicationRepository.getEventApplicationsByVolunteerIdAndSessionId(volunteerId, sessionId).isPresent()) {
+            throw new AppException(EventErrorCode.ALREADY_APPLIED);
+        }
 
         //check expect Vol amount
+        if (session.getExpectedVolAmount() == session.getApprovedApplicationCount()){
+            throw new AppException(EventErrorCode.EVENT_SESSION_FULL);
+        }
+
+        Event event = eventRepository.findById(sessionId).orElseThrow(
+                () -> new AppException(EventErrorCode.EVENT_NOT_EXISTED)
+        );
+
+        //check registration deadline
+        if (LocalDate.now().isAfter(event.getRecruitmentEndDate())) {
+            throw new AppException(EventErrorCode.EVENT_RECRUITMENT_CLOSED);
+        }
+
+        EventApplication eventApplication = new EventApplication();
+        eventApplication.setSession(session);
+        eventApplication.setVolunteer(volunteerRepository.getReferenceById(volunteerId));
 
         //check auto approve
-
-
-        //send notification to host
-
-
-
+        if (event.isAutoApprove()){
+            eventApplication.setStatus(EEventApplicationStatus.APPROVED);
+            session.setApprovedApplicationCount(session.getApprovedApplicationCount()+1);
+            eventSessionRepository.save(session);
+            eventApplicationRepository.save(eventApplication);
+            if (session.getExpectedVolAmount() == session.getApprovedApplicationCount()){
+                //todo send notification to host
+            }
+        } else {
+            eventApplication.setStatus(EEventApplicationStatus.PENDING);
+            eventApplicationRepository.save(eventApplication);
+            // todo send notification to host
+        }
     }
 }
