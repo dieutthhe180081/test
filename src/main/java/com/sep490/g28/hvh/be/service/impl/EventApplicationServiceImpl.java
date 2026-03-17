e78bf065abce4595a8b79a108142c1c21ac49686
huyendieu8304 <huyendieu8304@gmail.com>
2026-03-17 17:22:51 +0700

    add: check overlap apply session

--- src/main/java/com/sep490/g28/hvh/be/service/impl/EventApplicationServiceImpl.java
+++ src/main/java/com/sep490/g28/hvh/be/service/impl/EventApplicationServiceImpl.java
@@ -2,13 +2,13 @@ package com.sep490.g28.hvh.be.service.impl;
 
 import com.sep490.g28.hvh.be.auth.CurrentUserProvider;
 import com.sep490.g28.hvh.be.constant.EEventApplicationStatus;
+import com.sep490.g28.hvh.be.constant.EEventStatus;
 import com.sep490.g28.hvh.be.entity.Event;
 import com.sep490.g28.hvh.be.entity.EventApplication;
 import com.sep490.g28.hvh.be.entity.EventSession;
 import com.sep490.g28.hvh.be.exception.AppException;
 import com.sep490.g28.hvh.be.exception.errorCodeImpl.EventErrorCode;
 import com.sep490.g28.hvh.be.repository.EventApplicationRepository;
-import com.sep490.g28.hvh.be.repository.EventRepository;
 import com.sep490.g28.hvh.be.repository.EventSessionRepository;
 import com.sep490.g28.hvh.be.repository.VolunteerRepository;
 import com.sep490.g28.hvh.be.service.EventApplicationService;
@@ -29,9 +29,9 @@ import java.util.UUID;
 public class EventApplicationServiceImpl implements EventApplicationService {
     EventSessionRepository eventSessionRepository;
     EventApplicationRepository eventApplicationRepository;
+    VolunteerRepository volunteerRepository;
+
     CurrentUserProvider currentUserProvider;
-    private final EventRepository eventRepository;
-    private final VolunteerRepository volunteerRepository;
 
     @Transactional
     @Override
@@ -40,44 +40,51 @@ public class EventApplicationServiceImpl implements EventApplicationService {
         EventSession session = eventSessionRepository.findById(sessionId).orElseThrow(
                 () -> new AppException(EventErrorCode.EVENT_SESSION_NOT_EXISTED));
 
-        UUID volunteerId = currentUserProvider.getId();
-
-        //Have ever the volunteer applied for this session yet?
-        if (eventApplicationRepository.getEventApplicationsByVolunteerIdAndSessionId(volunteerId, sessionId).isPresent()) {
-            throw new AppException(EventErrorCode.ALREADY_APPLIED);
+        Event event = session.getEvent();
+        //only allow application when the event status is RECRUITING
+        if (!event.getStatus().equals(EEventStatus.RECRUITING)){
+            throw new AppException(EventErrorCode.EVENT_NOT_RECRUITING);
         }
-
-        //check expect Vol amount
-        if (session.getExpectedVolAmount() == session.getApprovedApplicationCount()){
-            throw new AppException(EventErrorCode.EVENT_SESSION_FULL);
-        }
-
-        Event event = eventRepository.findById(sessionId).orElseThrow(
-                () -> new AppException(EventErrorCode.EVENT_NOT_EXISTED)
-        );
 
         //check registration deadline
         if (LocalDate.now().isAfter(event.getRecruitmentEndDate())) {
             throw new AppException(EventErrorCode.EVENT_RECRUITMENT_CLOSED);
         }
 
+        UUID volunteerId = currentUserProvider.getId();
+        //Have ever the volunteer applied for this session yet?
+        if (eventApplicationRepository.getEventApplicationsByVolunteerIdAndSessionId(volunteerId, sessionId).isPresent()) {
+            throw new AppException(EventErrorCode.ALREADY_APPLIED);
+        }
+        //check expected Vol amount
+        if (session.getExpectedVolAmount() == session.getApprovedApplicationCount()){
+            throw new AppException(EventErrorCode.EVENT_SESSION_FULL);
+        }
+
+        //check the session overlap with any applied session
+        if (eventApplicationRepository.findOverlapSession(
+                volunteerId,
+                session.getStartDateTime().toLocalDate(),
+                session.getStartDateTime(),
+                session.getEndDateTime()
+        ) != null
+        ) {
+            throw new AppException(EventErrorCode.APPLYING_SESSION_TIME_CONFLICT);
+        }
+
         EventApplication eventApplication = new EventApplication();
         eventApplication.setSession(session);
         eventApplication.setVolunteer(volunteerRepository.getReferenceById(volunteerId));
+        eventApplication.setSessionDate(session.getStartDateTime().toLocalDate());
 
         //check auto approve
         if (event.isAutoApprove()){
             eventApplication.setStatus(EEventApplicationStatus.APPROVED);
             session.setApprovedApplicationCount(session.getApprovedApplicationCount()+1);
             eventSessionRepository.save(session);
-            eventApplicationRepository.save(eventApplication);
-            if (session.getExpectedVolAmount() == session.getApprovedApplicationCount()){
-                //todo send notification to host
-            }
         } else {
             eventApplication.setStatus(EEventApplicationStatus.PENDING);
-            eventApplicationRepository.save(eventApplication);
-            // todo send notification to host
         }
+        eventApplicationRepository.save(eventApplication);
     }
 }
